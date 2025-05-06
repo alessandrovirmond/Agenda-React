36e46285ee099fc7e4dcef2cbc6c092dc8abbbf5
Alessandro Virmond <avirmond2000@gmail.com>
2025-05-06 17:16:27 -0300

    debugObserver adicionado para depuracao

--- src/state/seletores/index.ts
+++ src/state/seletores/index.ts
@@ -1,20 +1,34 @@
 import { selector } from "recoil";
 import { filtroDeEventos, listaDeEventosState } from "../atom";
+import { IEvento } from "../../interfaces/IEvento";
 
-const eventosFiltradosState = selector({
-    key: 'eventosFiltradosState',
-    get: ({ get }) => {
-        const filtro = get(filtroDeEventos)
-        const todosOsEventos = get(listaDeEventosState)
-        const eventos = todosOsEventos.filter(evento => {
-            if (!filtro.data) {
-              return true
-            }
-            const ehOMesmoDia = filtro.data.toISOString().slice(0, 10) === evento.inicio.toISOString().slice(0, 10)
-            return ehOMesmoDia
-          })
-        return eventos
-    }
-})
+export const eventosFiltradosState = selector({
+  key: "eventosFiltradosState",
+  get: ({ get }) => {
+    const filtro = get(filtroDeEventos);
+    const todosOsEventos = get(listaDeEventosState);
+    const eventos = todosOsEventos.filter((evento) => {
+      if (!filtro.data) {
+        return true;
+      }
+      const ehOMesmoDia =
+        filtro.data.toISOString().slice(0, 10) ===
+        evento.inicio.toISOString().slice(0, 10);
+      return ehOMesmoDia;
+    });
+    return eventos;
+  },
+});
 
-export default eventosFiltradosState
+export const eventosAsync = selector({
+  key: "eventosAsync",
+  get: async () => {
+    const response = await fetch("http://localhost:8000/eventos");
+    const eventosJson: IEvento[] = await response.json();
+    return eventosJson.map((e) => ({
+      ...e,
+      inicio: new Date(e.inicio),
+      fim: new Date(e.fim)
+    }));
+  },
+});
